4403fe481cf79b6d855a515b4856bf55cc83d1c4
Luis Ojeda <lojedap@medialabla.com>
2024-06-12 13:59:50 -0500

    ajuste router

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 import Home from "./views/Home.js/Home";
+import ErrorBoundary from "./views/ErrorBoundary/ErrorBoundary";
 import Details from "./views/Details/Details";
+// import { Children } from "react";
 
+const basename = process.env.NODE_ENV === 'production' ? "/react-project-pet-adoption" : "";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/details/:petId",
-    element: <Details />,
+    errorElement: <ErrorBoundary />,
+    children: [
+
+      {
+        path: "/details/:petId",
+        element: <Details />,
+      }
+
+    ]
   }
+
 ];
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {basename});
 
 function App() {
   return (
